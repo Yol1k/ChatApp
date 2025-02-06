833d96d5841263785ff103dcaab324976ca58099
Yol1k <artemtimov@mail.ru>
2025-02-07 02:17:29 +0300

    first commit

---
+++ app/src/main/java/com/example/chatapp/ui/fragments/ChatsFragment.kt
@@ -0,0 +1,30 @@
+package com.example.chatapp.ui.fragments
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.chatapp.databinding.FragmentChatsBinding
+
+class ChatsFragment : Fragment() {
+    private var _binding: FragmentChatsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentChatsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.textView.text = "Чаты"
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
